63c8838f87c7e31d989dbd5bca5dee1975f3f9d0
[CLOUD4] 신영광 <halogod9985@naver.com>
2024-07-05 00:59:56 +0900

    Critical 시스템 추가

--- src/prototypeSystem/combat/CombatSystem.java
+++ src/prototypeSystem/combat/CombatSystem.java
@@ -2,8 +2,6 @@ package prototypeSystem.combat;
 
 import prototypeSystem.character.Enemy;
 import prototypeSystem.character.Player;
-import prototypeSystem.item.Armor;
-import prototypeSystem.item.Weapon;
 
 import java.util.Random;
 
@@ -17,7 +15,8 @@ public class CombatSystem {
     private int magicAttack;
     private int defense;
     private int magicDefense;
-    private int critical;
+    private int criticalChance;
+    private int criticalMult;
     private int dodge;
     private int accuracy;
     private int attackSpeed;
@@ -32,7 +31,8 @@ public class CombatSystem {
     private int eMagicAttack;
     private int eDefense;
     private int eMagicDefense;
-    private int eCritical;
+    private int eCriticalChance;
+    private int eCriticalMult;
     private int eDodge;
     private int eAccuracy;
     private int eAttackSpeed;
@@ -49,36 +49,44 @@ public class CombatSystem {
         int count = 1;
         while (true) {
             System.out.println("=====================================================");
-            System.out.println(count + "턴");
-            // if(playerAttackSpeed >= enemyAttackSpeed){  //공격속도 추가 예정
 
-            //크리티컬 적용 예정
+            System.out.println(count + "턴");
 
             System.out.println(Name + ", 체력 : " + currentHP + "/" + MaxHP);
+            // 데미지 정의
             int currentAttack1 = attack + (int) (additionalDamage() * attack * 0.01) - eDefense;
             if (currentAttack1 < 1) {
                 currentAttack1 = 1;
             }
 
-            System.out.print(Name + "의 공격 : "); //playerAttack 에 디폴트 스킬 장착예정
+            System.out.println(Name + "의 " + "공격" ); //playerAttack 에 디폴트 스킬 장착예정
+
+            //회피  정의
             if (playerHitChance()) {
                 System.out.println(eName + ", 공격을 회피했다.");
-            } else {
-                System.out.println(currentAttack1 + "의 피해를 입혔다.");
+            }
+            //크리티컬 정의
+            else if (playerCriticalChance()) {
+                System.out.println((currentAttack1*criticalMult)+ "의 피해");
+                eCurrentHP -= currentAttack1*criticalMult;
+            }
+            //평타
+            else {
+                System.out.println(currentAttack1 + "의 피해");
                 eCurrentHP -= currentAttack1;
             }
 
 
-            System.out.println("                              " + eName + ", 체력 : " + eCurrentHP + "/" + eMaxHP);
+            System.out.println("                                      " + eName + " " + eCurrentHP + " / " + eMaxHP);
             if (eCurrentHP <= -0) {
 
-
+            // 적 사망 정의
                 System.out.println("적 사망. \n" + enemy.getEXP() + " 경험치 획득");
 
                 if(enemy.getBoss()==1 && player.getMapUnlock()== stageChoice){
                     System.out.println("보스처치.. 다음지역이 해금됩니다..");
                     System.out.println("상점에 새로운 물건들이 보급됩니다.");
-                    player.setMapUnlock(2);
+                    player.setMapUnlock(player.getMapUnlock() + 1);
                 }
 
                 //아이템 드랍 추가예정
@@ -88,23 +96,31 @@ public class CombatSystem {
                 break;
 
             }
-
+            // 적 턴 시작
+            System.out.println("-----------------------------------------------------");
             int currentAttack2 = eAttack + (int) (additionalDamage() * eAttack * 0.01) - defense;
             if (currentAttack2 < 1) {
                 currentAttack2 = 1;
             }
-            System.out.print("                              " + eName + "의 공격 : ");
+            System.out.println("                                          " + eName + "의 " + "공격");
             if (enemyHitChance()) {
-                System.out.println(Name + ", 공격을 회피했다.");
-            } else {
-                System.out.println(currentAttack2 + "의 피해를 입혔다.");
+                System.out.println("                                   " +Name + ", 공격을 회피했다.");
+            }
+            else if (enemyCriticalChance()) {
+
+                System.out.println(("                                            " + currentAttack2*eCriticalMult)+ "의 피해");
+                currentHP -= currentAttack2*eCriticalMult;
+
+            }
+            else {
+                System.out.println("                                              " + currentAttack2 + "의 피해");
                 currentHP -= currentAttack2;
             }
-            System.out.println(Name + ", 체력 : " + currentHP + "/" + MaxHP);
+            System.out.println(Name + " " + currentHP + " / " + MaxHP);
             if (currentHP <= -0) {
                 System.out.println("당신은 사망하였습니다. 경험치의 일부를 잃어버립니다.");
                 player.setDeathCount(player.getDeathCount() + 1);
-                player.setCurrentEXP(player.getCurrentEXP() / 4);
+              //  player.setCurrentEXP(player.getCurrentEXP() / 4);
                 break;
             }
             System.out.println(count + "턴 종료");
@@ -118,6 +134,7 @@ public class CombatSystem {
 
     }
 
+    //startCombat의 데이터를 이쪽으로 옮겨야 함.
     public void startShortCombat(Player player, Enemy enemy, int stageChoice) {
         getCombatStatus(player, enemy);
         int count = 1;
@@ -135,8 +152,8 @@ public class CombatSystem {
 
                 System.out.print("적 사망.                 " + enemy.getEXP() + " 경험치 획득");
                 if(enemy.getBoss()==1 && player.getMapUnlock()== stageChoice){
-                    System.out.print("             보스처치.. 다음지역이 해금됩니다..");
-                    player.setMapUnlock(1);
+                    System.out.print("                                          보스처치.. 다음지역이 해금됩니다..");
+                    player.setMapUnlock(player.getMapUnlock() + 1);
                 }
 
                 shortGoldChance(player, enemy);
@@ -191,12 +208,13 @@ public class CombatSystem {
         Name = player.getName();
         MaxHP = player.getMaxHP() + player.getAddHP();
         currentHP = MaxHP;
-        attack = (STR * 3) + DEX;
+        attack = (STR * 2) + DEX;
         magicAttack = INT * 3;
-        defense = STR * 2;
+        defense = STR;
         magicDefense = INT * 2;
 
-        critical = DEX;
+        criticalChance = (int) (0.5 * DEX);
+        criticalMult = 3 + (int)(0.01*DEX);
         dodge = DEX;
         attackSpeed = DEX;
 
@@ -216,12 +234,13 @@ public class CombatSystem {
         eMaxHP = enemy.getMaxHP();
         eCurrentHP = eMaxHP;
 
-        eAttack = (enemy.getSTR() * 3) + enemy.getDEX();
+        eAttack = (enemy.getSTR() * 2) + enemy.getDEX();
         eMagicAttack = enemy.getINT() * 3;
-        eDefense = enemy.getSTR() * 2;
+        eDefense = enemy.getSTR();
         eMagicDefense = enemy.getINT() * 2;
 
-        eCritical = enemy.getDEX();
+        eCriticalChance = (int)(enemy.getDEX()*0.5);
+        eCriticalMult = 3 + (int)(0.01*enemy.getDEX());
         eDodge = enemy.getDEX();
         eAttackSpeed = enemy.getDEX();
 
@@ -237,7 +256,7 @@ public class CombatSystem {
     public int additionalDamage() {
         int additionalDamage = 0;
         int randomValue1 = rand.nextInt(2);  // 난수가 음수일지, 양수일지를 선택해주는 메서드
-        int randomValue2 = rand.nextInt(40); // 추가데미지 난수
+        int randomValue2 = rand.nextInt(30); // 추가데미지 난수
         if (randomValue1 == 0) {
             additionalDamage = (randomValue2 * -1);
         } else {
@@ -265,6 +284,24 @@ public class CombatSystem {
             return true;
         }
 
+    }
+
+    public boolean playerCriticalChance() {
+        int random = rand.nextInt(100);
+        if (random + criticalChance >= 100) {
+            System.out.println("치명타!");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean enemyCriticalChance(){
+        int random = rand.nextInt(100);
+        if (random + eCriticalChance >= 100) {
+            System.out.println("                                                " +"치명타!");
+            return true;
+        }
+        return false;
     }
 
     public void goldChance(Player player, Enemy enemy) {
@@ -298,7 +335,7 @@ public class CombatSystem {
         System.out.println("  | 마법방어력 : " + magicDefense);
         System.out.print("회피율 : " + dodge);
         System.out.println("  | 명중률 : " + accuracy);
-        System.out.println("치명타 확률 :" + critical);
+        System.out.println("치명타 확률 :" + criticalChance);
         System.out.println();
         System.out.println("레벨업 필요 경험치 : " + (player.getLevel()*player.getLevel()*3));
         System.out.println("현재 경험치 : " + player.getCurrentEXP());
